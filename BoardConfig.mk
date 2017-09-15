c8326d4066e731553ee1c12fca7899c38b48ec9f
sub77 <sub77@ymail.com>
2017-09-15 07:58:51 +0000

    falcon: update prebuilt kernel image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,10 +17,27 @@ ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Kernel Prebuilt
 TARGET_PREBUILT_KERNEL := device/motorola/falcon/zImage-dtb
+ifdef TARGET_PREBUILT_KERNEL
 BOARD_CUSTOM_BOOTIMG_MK := device/motorola/falcon/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom vmalloc=400M androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
+endif
+
+# Kernel Inline
+ifndef TARGET_PREBUILT_KERNEL
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=qcom vmalloc=400M utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := twrp_falcon_defconfig
+TARGET_KERNEL_SOURCE := kernel/motorola/msm8226
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
+endif
+
+LZMA_RAMDISK_TARGETS := recovery
 
 # Init
 TARGET_INCREASES_COLDBOOT_TIMEOUT := true
@@ -41,6 +58,7 @@ BOARD_SUPPRESS_SECURE_ERASE := true
 
 # TWRP
 TW_DEFAULT_EXTERNAL_STORAGE := true
+TW_DEVICE_VERSION := 1
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXCLUDE_SUPERSU := true
 TW_IGNORE_MAJOR_AXIS_0 := true
